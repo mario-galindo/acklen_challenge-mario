9af48e847dccf015fbef011471ee076e357f1786
mario-galindo <mgalindo9410@gmail.com>
2020-09-24 12:35:05 -0600

    add azure AD Directives to index file app

--- client/carscollection/src/index.js
+++ client/carscollection/src/index.js
@@ -3,12 +3,14 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import AzureAD from "react-aad-msal";
+import { signInAuthProvider} from "./authProvider";
 
 ReactDOM.render(
-  <React.StrictMode>
+  <AzureAD provider={signInAuthProvider} forceLogin={true}>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </AzureAD>,
+  document.getElementById("root")
 );
 
 // If you want your app to work offline and load faster, you can change
