fc887cb5cd4c3ab1f8265d27c2ccc1c67d3da438
mario-galindo <mgalindo9410@gmail.com>
2020-09-24 22:02:42 -0600

    add call to api from get data

--- client/carscollection/src/Views/Home.js
+++ client/carscollection/src/Views/Home.js
@@ -1,12 +1,25 @@
 import React from "react";
+import axios from "axios";
+import { UseAxiosGet } from "../Hooks/HttpRequests";
 
 function Home() {
-
   const url = `https://secureapi-galindo.azurewebsites.net/Item`;
+
+  let cars = UseAxiosGet(url);
+  let content = null;
+
+  if (cars.data) {
+    content = cars.data.map((car, key) => (
+      <div key={key}>
+        <h2>{car.name}</h2>
+      </div>
+    ));
+  }
 
   return (
     <div className="px-3">
       <h5>Your classis cars</h5>
+      {content}
     </div>
   );
 }
