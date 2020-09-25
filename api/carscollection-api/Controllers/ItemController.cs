761d366ad3f65df7396d6371ac088c3b65994f38
mario-galindo <mgalindo9410@gmail.com>
2020-09-24 22:39:22 -0600

    generate publish artifact

---
+++ api/carscollection-api/Controllers/ItemController.cs
@@ -0,0 +1,75 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.Azure.Documents;
+using Microsoft.Azure.Documents.Client;
+using Microsoft.Extensions.Configuration;
+using carscollection_api.Models;
+
+namespace carscollection_api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ItemController : ControllerBase
+    {
+        private readonly IDocumentClient _documentClient;
+        readonly string databaseId;
+        readonly string collectionId;
+        public IConfiguration Configuration { get; }
+        public ItemController(IDocumentClient documentClient, IConfiguration configuration)
+        {
+            _documentClient = documentClient;
+            Configuration = configuration;
+
+            databaseId = Configuration["databaseId"];
+            collectionId = "Items";
+
+            BuildCollection().Wait();
+        }
+
+        private async Task BuildCollection()
+        {
+            await _documentClient.CreateDatabaseIfNotExistsAsync(new Database { Id = databaseId });
+            await _documentClient.CreateDocumentCollectionIfNotExistsAsync(UriFactory.CreateDatabaseUri(databaseId), new DocumentCollection { Id = collectionId });
+        }
+
+        [HttpGet]
+        public IQueryable<Item> Get()
+        {
+            return _documentClient.CreateDocumentQuery<Item>(UriFactory.CreateDocumentCollectionUri(databaseId, collectionId), new FeedOptions { MaxItemCount = 20 });
+        }
+
+        [HttpGet("{id}")]
+        public IQueryable<Item> Get(string id)
+        {
+            return _documentClient.CreateDocumentQuery<Item>(UriFactory.CreateDocumentCollectionUri(databaseId, collectionId), new FeedOptions { MaxItemCount = 20 })
+            .Where((i) => i.Id == id);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Post([FromBody] Item item)
+        {
+            var response = await _documentClient.CreateDocumentAsync(UriFactory.CreateDocumentCollectionUri(databaseId, collectionId), item);
+            return Ok();
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> Put(string id, [FromBody] Item item)
+        {
+            await _documentClient.ReplaceDocumentAsync(UriFactory.CreateDocumentUri(databaseId, collectionId, id),
+                item);
+            return Ok();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(string id)
+        {
+            await _documentClient.DeleteDocumentAsync(UriFactory.CreateDocumentUri(databaseId, collectionId, id));
+            return Ok();
+        }
+    }
+}
---
+++ api/carscollection-api/Models/Item.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace carscollection_api.Models
+{
+    public class Item
+    {
+        [Newtonsoft.Json.JsonProperty(PropertyName = "id")]
+        public string Id { get; set; }
+        [Newtonsoft.Json.JsonProperty(PropertyName = "name")]
+        public string Name { get; set; }
+        [Newtonsoft.Json.JsonProperty(PropertyName = "description")]
+        public string Description { get; set; }
+    }
+}
