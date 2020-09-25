fe800201a66ea274b2ef440e7fae573686dbb5f1
mario-galindo <mgalindo9410@gmail.com>
2020-09-24 21:33:55 -0600

    add form car

--- client/carscollection/src/Views/Car.js
+++ client/carscollection/src/Views/Car.js
@@ -3,7 +3,22 @@ import React from "react";
 function Car() {
   return (
     <div className="px-3">
-      <h3>Create a new car 🚗</h3>
+      <h5>CREATE A NEW CAR - 🚗</h5>
+
+      <form>
+        <div className="form-group">
+          <label for="InputCar">Brand</label>
+          <input type="text" className="form-control" id="InputCar" />
+        </div>
+        <div className="form-group">
+          <label for="InputYear">Year</label>
+          <input type="text" className="form-control" id="InputYear" />
+        </div>
+
+        <button type="submit" className="btn btn-primary">
+          Save
+        </button>
+      </form>
     </div>
   );
 }
