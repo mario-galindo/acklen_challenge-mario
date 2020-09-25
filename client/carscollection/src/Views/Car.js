96a500adbd3baeccdac17a428d951fadedf654ba
mario-galindo <mgalindo9410@gmail.com>
2020-09-24 21:12:15 -0600

    edit car view

--- client/carscollection/src/Views/Car.js
+++ client/carscollection/src/Views/Car.js
@@ -1,7 +1,11 @@
 import React from "react";
 
 function Car() {
-  return <div>Car</div>;
+  return (
+    <div className="px-3">
+      <h5>Create a new car</h5>
+    </div>
+  );
 }
 
 export default Car;
