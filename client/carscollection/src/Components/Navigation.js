cd5e29a63a1ec027645cf21f1dd47d85dd633e6f
mario-galindo <mgalindo9410@gmail.com>
2020-09-26 01:51:15 -0600

    change icons in navbar

--- client/carscollection/src/Components/Navigation.js
+++ client/carscollection/src/Components/Navigation.js
@@ -7,31 +7,48 @@ function Navigation() {
     <div>
       <nav className="navbar navbar-expand-lg navbar-light">
         <div className="navbar-brand">
-          {/* <Link to="/">Cars Collection Acklen</Link> */}
-          <svg
-            width="1em"
-            height="1em"
-            viewBox="0 0 16 16"
-            class="bi bi-house-fill"
-            fill="#806cef"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path
-              fill-rule="evenodd"
-              d="M8 3.293l6 6V13.5a1.5 1.5 0 0 1-1.5 1.5h-9A1.5 1.5 0 0 1 2 13.5V9.293l6-6zm5-.793V6l-2-2V2.5a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5z"
-            />
-            <path
-              fill-rule="evenodd"
-              d="M7.293 1.5a1 1 0 0 1 1.414 0l6.647 6.646a.5.5 0 0 1-.708.708L8 2.207 1.354 8.854a.5.5 0 1 1-.708-.708L7.293 1.5z"
-            />
-          </svg>
+          <Link to="/">
+            <button className="btn btn-outline-light">
+              <svg
+                width="1em"
+                height="1em"
+                viewBox="0 0 16 16"
+                class="bi bi-house-fill"
+                fill="#806cef"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  fill-rule="evenodd"
+                  d="M8 3.293l6 6V13.5a1.5 1.5 0 0 1-1.5 1.5h-9A1.5 1.5 0 0 1 2 13.5V9.293l6-6zm5-.793V6l-2-2V2.5a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5z"
+                />
+                <path
+                  fill-rule="evenodd"
+                  d="M7.293 1.5a1 1 0 0 1 1.414 0l6.647 6.646a.5.5 0 0 1-.708.708L8 2.207 1.354 8.854a.5.5 0 1 1-.708-.708L7.293 1.5z"
+                />
+              </svg>
+            </button>
+          </Link>
         </div>
 
         <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
           <div className="navbar-nav">
             <a className="nav-item nav-link active" href="/#">
-              <Link to="Car" style={{ color: "#806cef" }}>
-                Create
+              <Link to="Car">
+                <button className="btn btn-outline-light">
+                  <svg
+                    width="1em"
+                    height="1em"
+                    viewBox="0 0 16 16"
+                    class="bi bi-plus-square-fill"
+                    fill="#806cef"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      fill-rule="evenodd"
+                      d="M2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2zm6.5 4.5a.5.5 0 0 0-1 0v3h-3a.5.5 0 0 0 0 1h3v3a.5.5 0 0 0 1 0v-3h3a.5.5 0 0 0 0-1h-3v-3z"
+                    />
+                  </svg>
+                </button>
               </Link>
             </a>
           </div>
@@ -40,19 +57,21 @@ function Navigation() {
           <strong style={{ color: "#806cef" }} className="mr-sm-2">
             Sign In
           </strong>
-          <svg
-            width="1em"
-            height="1em"
-            viewBox="0 0 16 16"
-            class="bi bi-person-fill"
-            fill="#806cef"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path
-              fill-rule="evenodd"
-              d="M3 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1H3zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6z"
-            />
-          </svg>
+          <button className="btn btn-outline-light">
+            <svg
+              width="1em"
+              height="1em"
+              viewBox="0 0 16 16"
+              class="bi bi-person-fill"
+              fill="#806cef"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                fill-rule="evenodd"
+                d="M3 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1H3zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6z"
+              />
+            </svg>
+          </button>
         </form>
       </nav>
     </div>
--- client/carscollection/src/Views/Home.js
+++ client/carscollection/src/Views/Home.js
@@ -19,20 +19,20 @@ function Home(props) {
   return (
     <div className=" container px-3">
       <div className="jumbotron" style={{ backgroundColor: "#806cef" }}>
-        <h3 className="display-4" style={{ color: "#fff" }}>
+        <h4 className="display-4" style={{ color: "#fff",fontSize:"30px" }}>
           Classic Car Collection Tracker
-        </h3>
+        </h4>
         <p className="lead" style={{ color: "#fff" }}>
           This tool is designed for all{" "}
-          <strong style={{ fontWeight: "bold" }}>ackleners</strong> for register
+          <strong style={{ fontWeight: "bold" }}>Ackleners</strong> for register
           and share classic cars collections
         </p>
         <hr className="my-4" style={{ backgroundColor: "#fff" }} />
-        <p>
+        <p >
           <h5>
             <strong style={{ color: "#fff" }}>Summary Collection</strong>
           </h5>
-          <div className="row align-item-center justify-content-center">
+          <div className="row">
             <div
               className="card mt-3 mr-5"
               style={{ width: "18em", height: "7em" }}
@@ -58,7 +58,13 @@ function Home(props) {
         </p>
       </div>
       <h5 style={{ color: "#806cef" }}>
-        <strong>Yours friends classis cars</strong>
+        <strong>Classic car brands of your friends</strong>
+        <ul className="list-group list-group-horizontal mt-3">
+          <li className="list-group-item">Ford</li>
+          <li className="list-group-item">Chevy</li>
+          <li className="list-group-item">Ferrari</li>
+          <li className="list-group-item">Toyota</li>
+        </ul>
       </h5>
       {content}
     </div>
