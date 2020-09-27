abb90760eb6c7b57ee114ce940bdfb16b52efcc9
mario-galindo <mgalindo9410@gmail.com>
2020-09-26 19:57:30 -0600

    add methods to handle authentications

--- client/carscollection/src/Components/Navigation.js
+++ client/carscollection/src/Components/Navigation.js
@@ -1,8 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
-import {signIn} from '../Auth/authPopup'
+import {
+  client as myMSALObj,
+  request as loginRequest,
+} from "../Auth/authProvider";
 
 function Navigation() {
+  const [userName, SetUserName] = useState("");
+
+  const handleLogin = () => {
+    myMSALObj
+      .loginPopup(loginRequest)
+      .then((response) => {
+        SetUserName(myMSALObj.getAccount().name);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  const handleLogout = () => {
+    myMSALObj.logout();
+  };
+
   return (
     <nav className="navbar navbar-expand-lg bg-primary">
       <div className="navbar-brand" style={{ color: "#FFFFFF" }}>
@@ -10,10 +30,20 @@ function Navigation() {
       </div>
 
       <div className="collapse navbar-collapse" id="navbarNavAltMarkup"></div>
-      
-        <button className="btn btn-success mr-3" onClick={signIn}>Sign In</button>
-        <button className="btn btn-danger">Sign Out</button>
-      
+
+      <strong className="mr-3" style={{ color: "#FFFFFF" }}>
+        {userName}
+      </strong>
+      <button
+        id="signin"
+        className="btn btn-success mr-3"
+        onClick={handleLogin}
+      >
+        Sign In
+      </button>
+      <button className="btn btn-danger" onClick={handleLogout}>
+        Sign Out
+      </button>
     </nav>
   );
 }
